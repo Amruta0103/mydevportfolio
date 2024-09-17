4fcf9e7f302cb95d608a746b1cffc1274d75dea7
Dev Amruta <amydharap@gmail.com>
2024-09-17 18:02:49 +0530

    Update:
    -> trial base wip

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App() {
         <div className="flex justify-start items-start w-full">
           <h1 className="text-6xl">Amruta Dharap</h1>
         </div>
-        <div onClick={()=>console.log("clicked")} className="bloby overflow-hidden m-4 md:m-auto h-full w-full md:max-h-[350px] md:max-w-[500px] max-h-[250px] max-w-[300px] cursor-pointer">
-          <div className="move h-full w-full"></div>
+        <div onClick={()=>console.log("clicked")} className="bloby overflow-hidden m-4 md:m-auto h-full w-full md:max-h-[350px] md:max-w-[400px] max-h-[250px] max-w-[300px] cursor-pointer">
+          <div className=" blobymove h-full w-full"></div>
         </div>
         <div className="flex justify-end items-end w-full">
           <h1 className="text-6xl">Frontend Developer</h1>
