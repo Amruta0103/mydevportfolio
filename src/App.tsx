0cad3cb0ddd69beb0a75920da3b7a39b444d6899
Dev Amruta <amydharap@gmail.com>
2024-09-25 23:02:31 +0530

    Update:
    -> Wip page structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [blue, setBlue] = useState(false);
+  const [red, setRed] = useState(false);
+  const [yellow, setYellow] = useState(false);
+  const [green, setGreen] = useState(false);
   return (
-    <div className="App h-screen w-full flex m-auto text-black bg-[#EED7FB]">
-      <div className="flex justify-center m-auto max-w-[800px] w-full max-h-[550px] h-full rounded-[52px] bloby bg-[#DA99FF] shadow-[10px_20px_24px_0_#982CD6]">
-        <div className="flex flex-col gap-8 justify-center items-center w-full">
-          <h1 className="text-6xl">Amruta Dharap</h1>
-          <h1 className="text-4xl">Frontend Developer</h1>
+    <div className="App flex justify-center items-center w-full h-screen bg-new">
+      <div className="flex flex-col gap-6 h-full py-4 md:py-28 px-4 max-w-[300px] max-h-[500px] sm:max-w-[600px] sm:max-h-[600px] md:max-w-[700px] lg:max-w-[900px] lg:max-h-[700px]">
+        <div className={`top ${blue || yellow  === true ? 'hidden': 'flex max-h-full max-w-full'}`}>
+          <div className={`${red === true ? 'h-full w-full z-20 bg-[#FF99A0] transition-all': 'w-full h-full flex justify-center items-center z-0'} ${green ? 'hidden': 'flex'}`}>
+            <div onClick={()=>setRed(!red)} className="cursor-pointer">Click here 2</div>
+          </div>
+          <div className={`${green === true ? 'h-full w-full z-20 bg-[#99FFC1] transition-all': 'w-full h-full flex justify-center items-center z-0'} ${red ? 'hidden': 'flex'}`}>
+            <div onClick={()=>setGreen(!green)} className="cursor-pointer">Click here 3</div>
+          </div>
+        </div>
+        <div className={`mid ${blue || red || yellow || green  === true ? 'hidden': 'flex'} justify-center m-auto max-w-[800px] w-full max-h-[500px] h-full cursor-pointer z-0`}>
+          <div className="flex flex-col gap-8 justify-center items-center w-full">
+            <h1 className="text-6xl">Amruta Dharap</h1>
+            <h1 className="text-4xl">Frontend Developer</h1>
+          </div>
+        </div>
+        <div className={`bottom ${red || green  === true ? 'hidden': 'flex'}`}>
+          <div className={`${blue === true ? 'h-full w-full z-20 bg-[#99DDFF] transition-all': 'w-full h-full flex justify-center items-center z-0'} ${yellow ? 'hidden': 'flex'}`}>
+            <div onClick={()=>setBlue(!blue)} className="cursor-pointer">Click here 1</div>
+          </div>
+          <div className={`${yellow === true ? 'h-full w-full z-20 bg-[#FFF599] transition-all': 'w-full h-full flex justify-center items-center z-0'} ${blue ? 'hidden': 'flex'}`}>
+            <div onClick={()=>setYellow(!yellow)} className="cursor-pointer">Click here 4</div>
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx,tsx,webp,png,jpg,jpeg,svg}"],
-  theme: {
-    extend:{
-      cursor:{
-        'ship': "url('./src/assets/logo512.png')"
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 }
 
