06e4f7ba31dc6289df9d08899d1e1ca2593e6e36
Dev Amruta <amydharap@gmail.com>
2024-12-10 13:05:06 +0530

    Update:
    -> updated images and links

--- src/components/projectsData.tsx
+++ src/components/projectsData.tsx
@@ -1,23 +1,35 @@
+import Calculator from '../assets/Calculator.svg';
+import Clock from '../assets/Clock.svg';
+import Lift from '../assets/Lift.svg';
+import Keyboard from '../assets/Keyboard.svg';
 const projectsData = [
   {
     'title': 'Calculator',
     'description': 'A calculator App that does basic calculations',
-    'image': '',
+    'image': Calculator,
+    'liveLink': 'https://simply-calcy.netlify.app/',
+    'gitLink': 'https://github.com/Amruta0103/calculator',
   },
   {
     'title': 'Count MyTime',
     'description': 'A countdown timer. I explored asynchronous functions and calculations for setting timers with various corner cases',
-    'image': '',
+    'image': Clock,
+    'liveLink': 'https://countmytime.netlify.app/',
+    'gitLink': 'https://github.com/Amruta0103/countmytime',
   },
   {
     'title': 'Lift Simulation',
     'description': 'An interactive, visual representation of how multiple lifts would work at different levels of floors',
-    'image': '',
+    'image': Lift,
+    'liveLink': 'https://lift-simulation-amruta.netlify.app/',
+    'gitLink': 'https://github.com/Amruta0103/lift-simulation',
   },
   {
     'title': 'Minion Traslator',
     'description': "A simple translator using an API that translate into and from English into Minion's language",
-    'image': '',
+    'image': Keyboard,
+    'liveLink': 'https://minion-speaks-banana.netlify.app/',
+    'gitLink': 'https://github.com/Amruta0103/Banana-speak',
   },
 ];
 
--- src/components/workCards.tsx
+++ src/components/workCards.tsx
@@ -1,4 +1,3 @@
-import grainscreen from '../assets/grainscreen.jpg';
 import projectsData from './projectsData';
  
 export default function WorkCards(){
@@ -6,10 +5,14 @@ export default function WorkCards(){
     <div className='flex flex-col gap-8 w-full justify-center items-center'>
       {projectsData.map((item:any)=>(
         <div className="flex flex-col xs:flex-row gap-4 justify-evenly items-center w-full h-fit xs:even:flex-row-reverse">
-        <img src={grainscreen} className="w-[300px] h-[300px] rounded-3xl" alt="img"/>
+        <img src={item.image} className="w-[200px] h-[200px] md:w-[300px] md:h-[300px] rounded-3xl object-fill" alt="img"/>
         <div className="flex flex-col gap-2 sm:gap-4 max-w-[350px]">
           <h4 className="font-fontJose">{item.title}</h4>
           <p className="font-fontJetMono text-base leading-[18px] sm:text-lg sm:leading-5">{item.description}</p>
+          <div className='flex gap-3 items-center'>
+          <a href={item.liveLink} className="cursor-pointer underline font-fontJose text-base leading-[18px]">Live Link</a>
+          <a href={item.gitLink} className="cursor-pointer underline font-fontJose text-base leading-[18px]">GitHub</a>
+          </div>
         </div>
         </div>
       ))}
