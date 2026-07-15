f8f5825729ae65d12b145b8558b0ba722ec6940b
Dev Amruta <71976347+Amruta0103@users.noreply.github.com>
2026-07-16 00:37:09 +0530

    New Game- Stand Here

--- src/components/projectsData.tsx
+++ src/components/projectsData.tsx
@@ -2,12 +2,15 @@ import Calculator from '../assets/Calculator.svg';
 import Clock from '../assets/Clock.svg';
 import Lift from '../assets/Lift.svg';
 import Keyboard from '../assets/Keyboard.svg';
+import StandHere from '../assets/StandHere.png';
+
 const projectsData = [
   {
-    'title': 'Business Intelligence Filter Dashboard',
-    'description': 'A calculator App that does basic A performance-focused dummy BI dashboard that mimics Amazon-like filters',
-    'liveLink': 'https://bidashboard-three.vercel.app/',
-    'gitLink': 'https://github.com/Amruta0103/bidashboard',
+    'title': 'Stand Here',
+    'image': StandHere,
+    'description': 'Stand Here is a short arcade game built in Unity where your goal is to find the randomly generated safe tile before the timer runs out.',
+    'liveLink': 'https://amruta0103.itch.io/stand-here',
+    'gitLink': 'https://github.com/Amruta0103/Stand-Here',
   },
   {
     'title': 'Calculator',
